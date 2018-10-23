69c6534be49932561604be2034335fefb5f7b700
artiomleo <34216326+artiomleo@users.noreply.github.com>
2018-10-23 17:44:35 +0300

    First commit

---
+++ src/test/java/com/track/AppTest.java
@@ -0,0 +1,20 @@
+package com.track;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
